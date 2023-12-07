bbfb22fe2540e96df64bd6d7880549369563073d
KevinVanphavong <kvanphavong@webqam.fr>
2023-12-07 11:56:22 +0100

    feat(twig): add filter for array_sum in twig

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('price', [$this, 'formatPrice']),
+            new TwigFilter('sum', 'array_sum'),
+        ];
+    }
+
+    public function formatPrice(float $number, int $decimals = 0, string $decPoint = '.', string $thousandsSep = ','): string
+    {
+        $price = number_format($number, $decimals, $decPoint, $thousandsSep);
+        $price = '$'.$price;
+
+        return $price;
+    }
+}
